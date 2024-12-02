c9a461dae7a59b5cf91df4630f3940cdf6c1138a
Adeyemo Oluwaseyifunmi <heasylife1234@gmail.com>
2024-12-02 06:17:55 +0100

    making a reusable custom hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,41 +1,47 @@
 import { useState, useEffect } from "react";
 
-const useFetch = async () => {
-  const [BlogsData, setBlogsData] = useState(null);
-  const [isPending, setisPending] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogsData(data);
-          setisPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setisPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+const useFetch =  (ur) => {
+   //   const [name, setName] = useState("oluwaseyi");
+   const [blogData, setBlogData] = useState(null);
+   const [isPending, setisPending] = useState(true);
+   const [error, setError] = useState(null);
+ 
 
-  // try {
-  //   console.log("data");
-  //   const response = await fetch("../data/db.json");
-  //   if (response.ok) {
-  //     const data = await response.json();
-  //     setBlogsData(data);
-  //   }
-  // } catch (error) {
-  //   console.log("an error occured");
-  //   setError(error);
-  // }
+   useEffect(() => {
+     setTimeout(() => {
+       fetch(url)
+         .then((res) => {
+           if (!res.ok) {
+             throw Error("could not fetch the data for that resource");
+           }
+           return res.json();
+         })
+         .then((data) => {
+           setBlogData(data);
+           setisPending(false);
+           setError(null);
+         })
+         .catch((err) => {
+           setisPending(false);
+           setError(err.message);
+         });
+     }, 1000);
+ 
+   }, [url]);
+
+//   // try {
+//   //   console.log("data");
+//   //   const response = await fetch("../data/db.json");
+//   //   if (response.ok) {
+//   //     const data = await response.json();
+//   //     setBlogsData(data);
+//   //   }
+//   // } catch (error) {
+//   //   console.log("an error occured");
+//   //   setError(error);
+//   // }
+
+return {blogData, isPending, error}
 };
 
 export default useFetch;
